f874467964ea9645367b2d57d7549e23d1ee1408
timDeHof <this.is.tim.dehof@gmail.com>
2023-07-30 16:53:23 -0400

    chore(Computers.jsx): optimize lighting in Computers component feat(Computers.jsx): add spot light with shadow casting capability refactor(Computers.jsx): remove unused castShadow and shadowMapSize properties

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -3,20 +3,27 @@ import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
 import CanvasLoader from "../Loader";
+
 const Computers = ({ isMobile }) => {
   const computer = useGLTF("./desktop_pc/scene.gltf");
   return (
     <mesh>
-      <hemisphereLight intensity={0.15} groundColor="black" />
+      <hemisphereLight intensity={2.3} groundColor="black" />
+
+      <spotLight
+        position={[-20, 50, 10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadow-MapSize={1024}
+      />
       <pointLight intensity={1} />
-      <spotLight position={[-20, 50, 10]} angle={0.12} penumbra={1} />
       <primitive
         object={computer.scene}
         scale={isMobile ? 0.7 : 0.75}
         position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
-        castShadow
-        shadowMapSize={1024}
       />
     </mesh>
   );
